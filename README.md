53988794a5c1859178a34c582584f69914f9eb1a
ArthurHlt <arthur.halet@orange.com>
2017-08-25 17:22:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# gitlab-accept-mr
+
+Accept automatically all opened merge request in a project on Gitlab.
+
+## Installation
+
+### On *nix system
+
+You can install this via the command-line with either `curl` or `wget`.
+
+#### via curl
+
+```bash
+$ sh -c "$(curl -fsSL https://raw.github.com/ArthurHlt/gitlab-accept-mr-cli/master/bin/install.sh)"
+```
+
+#### via wget
+
+```bash
+$ sh -c "$(wget https://raw.github.com/ArthurHlt/gitlab-accept-mr-cli/master/bin/install.sh -O -)"
+```
+
+### On windows
+
+You can install it by downloading the `.exe` corresponding to your cpu from releases page: https://github.com/ArthurHlt/gitlab-accept-mr-cli/releases .
+Alternatively, if you have terminal interpreting shell you can also use command line script above, it will download file in your current working dir.
+
+### From go command line
+
+Simply run in terminal:
+
+```bash
+$ go get github.com/ArthurHlt/gitlab-accept-mr-cli
+```
+
+## Usage
+
+```
+NAME:
+   accept-mr - Automatically accept Merge Request on project
+
+USAGE:
+   accept-mr [global options] command [command options] [arguments...]
+
+VERSION:
+   1.0.0
+
+COMMANDS:
+     help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --url value, -u value      Url to your gitlab [$GITLAB_URL]
+   --token value, -t value    User token to access the api [$GITLAB_TOKEN]
+   --project value, -p value  Project name where accepting mr (e.g.: owner/repo) [$GITLAB_PROJECT]
+   --failed-on-error, -e      If true accept in error exit with status code > 0
+   --insecure, -k             Ignore certificate validation
+   --log-json, -j             Write log in json
+   --no-color                 Logger will not display colors
+   --on-build-succeed, --bs   Merge request will automatically accepted if pipeline succeeded
+   --help, -h                 show help
+   --version, -v              print the version
+```
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"github.com/xanzy/go-gitlab"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "accept-mr"
+	app.Version = "1.0.0"
+	app.Usage = "Automatically accept Merge Request on project"
+	app.ErrWriter = os.Stderr
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "url, u",
+			Usage:  "Url to your gitlab",
+			EnvVar: "GITLAB_URL",
+		},
+		cli.StringFlag{
+			Name:   "token, t",
+			Usage:  "User token to access the api",
+			EnvVar: "GITLAB_TOKEN",
+		},
+		cli.StringFlag{
+			Name:   "project, p",
+			Usage:  "Project name where accepting mr (e.g.: owner/repo)",
+			EnvVar: "GITLAB_PROJECT",
+		},
+		cli.BoolFlag{
+			Name:  "failed-on-error, e",
+			Usage: "If true accept in error exit with status code > 0",
+		},
+		cli.BoolFlag{
+			Name:  "insecure, k",
+			Usage: "Ignore certificate validation",
+		},
+		cli.BoolFlag{
+			Name:  "log-json, j",
+			Usage: "Write log in json",
+		},
+		cli.BoolFlag{
+			Name:  "no-color",
+			Usage: "Logger will not display colors",
+		},
+		cli.BoolFlag{
+			Name:  "on-build-succeed, bs",
+			Usage: "Merge request will automatically accepted if pipeline succeeded",
+		},
+	}
+	app.Action = acceptMr
+	app.Run(os.Args)
+}
+func checkRequired(c *cli.Context) error {
+	if c.GlobalString("url") == "" {
+		return fmt.Errorf("Gitlab url can't be empty set with --url or GITLAB_URL env var")
+	}
+	if c.GlobalString("token") == "" {
+		return fmt.Errorf("Gitlab token can't be empty set with --token or GITLAB_TOKEN env var")
+	}
+	if c.GlobalString("project") == "" {
+		return fmt.Errorf("Gitlab project can't be empty set with --project or GITLAB_PROJECT env var")
+	}
+	return nil
+}
+func loadClient(c *cli.Context) (*gitlab.Client, error) {
+
+	token := c.GlobalString("token")
+	url := c.GlobalString("url")
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: c.GlobalBool("insecure"),
+		},
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		Proxy: http.ProxyFromEnvironment,
+	}
+	if !strings.HasSuffix(url, "/api/v4") {
+		url = strings.TrimSuffix(url, "/") + "/api/v4"
+	}
+	git := gitlab.NewClient(&http.Client{Transport: transport}, token)
+	err := git.SetBaseURL(url)
+	return git, err
+}
+func acceptMr(c *cli.Context) error {
+	loadLogConfig(c)
+	err := checkRequired(c)
+	if err != nil {
+		return err
+	}
+	client, err := loadClient(c)
+	if err != nil {
+		return err
+	}
+	projName := c.GlobalString("project")
+	state := "opened"
+	mrs, _, err := client.MergeRequests.ListMergeRequests(projName, &gitlab.ListMergeRequestsOptions{
+		State: &state,
+	})
+	if err != nil {
+		return err
+	}
+	nbErrors := 0
+	buildSucceed := c.GlobalBool("on-build-succeed")
+	options := &gitlab.AcceptMergeRequestOptions{}
+	if buildSucceed {
+		options.MergeWhenBuildSucceeds = &buildSucceed
+	}
+	for _, mr := range mrs {
+		entry := log.WithField("title", mr.Title)
+		if mr.WorkInProgress {
+			entry.Warn("Skipping merge request, it is in WIP")
+			continue
+		}
+		entry.Info("Accepting merge request ...")
+		_, _, err = client.MergeRequests.AcceptMergeRequest(projName, mr.IID, options)
+		if err != nil {
+			entry.Errorf("Error occurred while accepting: %s ", err.Error())
+			nbErrors++
+			continue
+		}
+		entry.Info("Finished accepting merge request ...")
+	}
+	if c.GlobalBool("failed-on-error") && nbErrors > 0 {
+		return fmt.Errorf("You have %d merge request which can't be accepted", nbErrors)
+	}
+	return nil
+}
+func loadLogConfig(c *cli.Context) {
+	if c.GlobalBool("log-json") {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: c.GlobalBool("no-color"),
+	})
+
+}
