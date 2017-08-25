2e6d7b783de7fd3811dc15e74552ae4f1a8af157
ArthurHlt <arthur.halet@orange.com>
2017-08-25 17:48:12 +0200

    fix install script

--- bin/install.sh
+++ bin/install.sh
@@ -39,7 +39,7 @@ FILENAME="${NAME}_${OS}_${ARCH}"
 if [[ "$OS" == "windows" ]]; then
     FILENAME="${FILENAME}.exe"
 fi
-LINK="https://github.com/${OWNER}/${REPO_NAME}/releases/download/v${VERSION}/${FILENAME}"
+LINK="https://github.com/${OWNER}/${REPO_NAME}/releases/download/${VERSION}/${FILENAME}"
 if [[ "$OS" == "windows" ]]; then
     FILEOUTPUT="${FILENAME}"
 else
